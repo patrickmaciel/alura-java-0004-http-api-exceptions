0100dfb92102356936ba3e9eb939949e269728b4
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-05-28 17:30:35 -0300

    feat: requesting the movie name using a Scanner and create a dynamic search URL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the movie title: ");
+    String movieTitle = scanner.nextLine();
+
+    String apiKey = "43e78112";
+    String apiUrl = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movieTitle;
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("http://www.omdbapi.com/?apikey=43e78112&t=Matrix"))
+        .uri(URI.create(apiUrl))
         .build();
 
     try {
