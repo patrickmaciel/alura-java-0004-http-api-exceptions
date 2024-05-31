13f4ec28d3b0b7c8cf500e3ea9ee638e96e1d1b2
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-05-31 01:01:17 -0300

    feat: update Person exercise to write the response on a json file

--- src/main/java/org/example/exercises/MainPerson.java
+++ src/main/java/org/example/exercises/MainPerson.java
@@ -1,16 +1,25 @@
 package org.example.exercises;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainPerson {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String jsonResponse = "{\"nome\":\"João\",\"idade\":\"25\",\"cidade\":\"São Paulo\"}";
 
-    Gson gson = new Gson();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     PersonJson personJson = gson.fromJson(jsonResponse, PersonJson.class);
     Person person = new Person(personJson);
     System.out.println("PersonJson: " + personJson);
     System.out.println("Person: " + person);
+
+    File file = new File("person.json");
+    FileWriter fileWriter = new FileWriter(file);
+    fileWriter.write(gson.toJson(person));
+    fileWriter.close();
   }
 }
